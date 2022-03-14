c64c177588616721f272fff21ad5fd70bb9d9ce9
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2022-03-14 16:11:25 -0600

    Interfaz

--- pro1.py
+++ pro1.py
@@ -1,15 +1,3 @@
 from Ventana import Inicio
 inicio = Inicio()
 inicio.iniciar()
-#from Componentes import Componentes
-#from Analizador import AnalizadorLexico
-#
-#cadena = open('entrada.form').read()
-#
-#lexico = AnalizadorLexico()
-#lexico.analizar(cadena)
-#lexico.imprimirTokens()
-#tokens = lexico.listaTokens
-#cmp = Componentes()
-#componentes = cmp.getComponentes(tokens)
-#print(componentes)
